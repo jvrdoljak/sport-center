3acbe64128b4bacb8a7b3ddc2c39005d8e6f1c45
jvrdoljak <vrdoljakjosip@outlook.com>
2025-03-24 19:11:53 +0100

    feat: remove config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
 import { APP_GUARD, APP_INTERCEPTOR } from "@nestjs/core";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
@@ -22,9 +21,6 @@ import { UsersModule } from "./users/users.module";
 
 @Module({
 	imports: [
-		ConfigModule.forRoot({
-			isGlobal: true,
-		}),
 		TypeOrmModule.forRootAsync({
 			useFactory: () => ({
 				type: "mysql",
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,8 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import { UsersModule } from "src/users/users.module";
+import { JWT_SECRET } from "../config";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { JwtStrategy } from "./strategies/jwt.strategy";
@@ -12,10 +12,8 @@ import { JwtStrategy } from "./strategies/jwt.strategy";
 		UsersModule,
 		PassportModule,
 		JwtModule.registerAsync({
-			imports: [ConfigModule],
-			inject: [ConfigService],
-			useFactory: (configService: ConfigService) => ({
-				secret: configService.get<string>("JWT_SECRET"),
+			useFactory: () => ({
+				secret: JWT_SECRET,
 				signOptions: { expiresIn: "1d" },
 			}),
 		}),
