e07ed9a92f99a2cbb95bf65fc157dd45e5fe1af2
jvrdoljak <vrdoljakjosip@outlook.com>
2025-03-24 13:43:25 +0100

    feat: add password validation

--- src/users/dto/createUser.dto.ts
+++ src/users/dto/createUser.dto.ts
@@ -1,5 +1,11 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+import {
+	IsEmail,
+	IsNotEmpty,
+	IsString,
+	Matches,
+	MinLength,
+} from "class-validator";
 
 export class CreateUserDto {
 	@ApiProperty({ example: "user@example.com" })
@@ -10,6 +16,16 @@ export class CreateUserDto {
 	@ApiProperty({ example: "password123" })
 	@IsNotEmpty()
 	@IsString()
-	@MinLength(8)
+	@MinLength(8, { message: "password must have at least 8 characters" })
+	@Matches(/(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-])/, {
+		message: "password must have at least one special symbol",
+	})
+	@Matches(/(?=.*\d)/, { message: "password must have at least one number" })
+	@Matches(/(?=.*[A-Z])/, {
+		message: "password must have at least one uppecase letter",
+	})
+	@Matches(/(?=.*[a-z])/, {
+		message: "password must have at least one lowercase letter",
+	})
 	password: string;
 }
